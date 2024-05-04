7a840bf39d3a120490375162dfbd3a8ec24b84fb
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-05-04 17:22:10 +0800

    c

--- hypre/install.sh
+++ hypre/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-apt update
-apt -y install cmake make gcc g++ gfortran libmpich-dev
+sudo apt update
+sudo apt -y install cmake make gcc g++ gfortran libmpich-dev
 
 cd src
 ./configure --prefix=$PWD/../../install/hypre_install/ 
