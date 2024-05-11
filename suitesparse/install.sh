bbcfb0964a6ea699792dcc0bd463daa4d433aef6
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-05-11 11:11:04 +0800

    c

--- suitesparse/install.sh
+++ suitesparse/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-apt update
-apt -y install cmake make gcc g++ gfortran libmpfr-dev libgmp-dev 
+sudo apt update
+sudo apt -y install cmake make gcc g++ gfortran libmpfr-dev libgmp-dev 
 
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../../install/suitesparse_install -DBLAS_openblas_LIBRARY=$PWD/../../install/openblas_install/lib/libopenblas.a -DLAPACK_openblas_LIBRARY=$PWD/../../install/openblas_install/lib/libopenblas.a
-make -j8
+make -j4
 make install
