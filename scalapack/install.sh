5da25adef40996588fd8cbd6ecef1486e6baef99
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-05-24 11:56:37 +0800

    c

--- scalapack/install.sh
+++ scalapack/install.sh
@@ -4,9 +4,12 @@ sudo apt update
 sudo apt -y install cmake
 sudo apt -y install make
 sudo apt -y install gcc g++ gfortran
+sudo apt -y install libmpich-dev
 
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../../install/scalapack_install
+cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../../install/scalapack_install \
+      -DBLAS_LIBRARIES=$PWD/../../install/lapack_install/lib/libblas.a \
+      -DLAPACK_LIBRARIES=$PWD/../../install/lapack_install/lib/liblapack.a
 make -j4
 make install
