281bb6c5073678dcebb997cca7153975ed3228c6
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-08-05 13:21:38 +0800

    add superlu_dist

---
+++ superlu_dist/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -rf build
+mkdir build
+cd build
+cmake .. \
+      -DTPL_ENABLE_PARMETISLIB=ON \
+      -DTPL_PARMETIS_LIBRARIES=ON \
+      -DTPL_PARMETIS_INCLUDE_DIRS="$PWD/../../../install/parmetis_install/include/;$PWD/../../../NSM/extern/ALE/install/metis/include/" \
+      -DTPL_PARMETIS_LIBRARIES="$PWD/../../../install/parmetis_install/lib/libparmetis.a;$PWD/../../../NSM/extern/ALE/install/metis/lib/libmetis.so" \
+      -DCMAKE_INSTALL_PREFIX=$PWD/../../install/superlu_dist_install
+make -j4
+make install
+
---
+++ superlu_dist/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -rf build
+rm -rf ../install/superlu_dist_install
