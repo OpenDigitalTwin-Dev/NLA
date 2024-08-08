43acab3b0349142b311e5cd7dd45527b1c46427a
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-08-08 16:02:25 +0800

    c

--- libceed/install.sh
+++ libceed/install.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt -y install cmake
 sudo apt -y install make
-sudo apt -y install gcc g++ gfortran
+sudo apt -y install gcc g++ gfortran liblapack-dev
 
 make 
 make prefix=$PWD/../install/libceed_install install
