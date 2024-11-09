0213dc88013d131a3281db4325838c39ccebb70c
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-11-09 20:12:30 +0800

    c

--- petsc/install.sh
+++ petsc/install.sh
@@ -5,8 +5,11 @@ sudo apt -y install cmake
 sudo apt -y install make
 sudo apt -y install gcc g++ gfortran python3 libmpich-dev liblapack-dev
 
-./configure --prefix=$PWD/../install/petsc_install --with-scalar-type=complex
-make
+./configure --prefix=$PWD/../install/petsc_install \
+	    --with-scalar-type=complex \
+	    --with-debugging=1 \
+	    --with-x=0
+make -j4 
 make PETSC_DIR=$PWD PETSC_ARCH=arch-linux-c-debug all
 make PETSC_DIR=$PWD PETSC_ARCH=arch-linux-c-debug install
 make PETSC_DIR=$PWD/../install/petsc_install PETSC_ARCH="" check
