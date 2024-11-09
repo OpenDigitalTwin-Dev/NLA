17f975f7f54b0e76c20b607dfcde90c759604a7f
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-11-09 15:25:03 +0800

    c

--- trilinos/install.sh
+++ trilinos/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-apt update
-apt -y install cmake
-apt -y install make
-apt -y install gcc g++ gfortran libmpich-dev
-apt -y install p7zip-full libblas-dev liblapack-dev libboost-dev libhdf5-dev libnetcdf-dev
+sudo apt update
+sudo apt -y install cmake
+sudo apt -y install make
+sudo apt -y install gcc g++ gfortran libmpich-dev
+sudo apt -y install p7zip-full libblas-dev liblapack-dev libboost-dev libhdf5-dev libnetcdf-dev
 
 rm -rf build
 rm -rf packages
