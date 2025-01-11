5685534b2daaed31f7abce9e46a52227362c1239
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-01-11 22:27:56 +0800

    c

--- petsc/install.sh
+++ petsc/install.sh
@@ -9,13 +9,12 @@ sudo apt -y install gcc g++ gfortran python3 libmpich-dev liblapack-dev libhdf5-
 	    --with-fftw=1 \
 	    --with-fftw-dir=$PWD/../../install/fftw_install/ \
 	    --with-hdf5=1 \
-	    --with-hdf5-dir=/usr/lib/x86_64-linux-gnu/hdf5/openmpi/ \
+	    --with-hdf5-dir=$PWD/../../install/hdf5_install/ \
 	    --with-hdf5-fortran-bindings \
 	    --with-zlib=1 \
 	    --with-blas-lib=$PWD/../install/lapack_install/lib/libblas.a \
 	    --with-lapack-lib=$PWD/../install/lapack_install/lib/liblapack.a \
-	    --with-mpi=1 \
-	    --with-fortran-bindings=1
+	    --with-mpi=1 
 
 make -j4 
 make PETSC_DIR=$PWD PETSC_ARCH=arch-linux-c-debug all
@@ -25,6 +24,9 @@ make PETSC_DIR=$PWD/../install/petsc_install PETSC_ARCH="" check
 # >>> hdf5 compiled by me can't work very strange very strange
 #	    --with-hdf5-dir=$PWD/../../install/hdf5_install/ \
     #           --with-hdf5-dir=/usr/lib/x86_64-linux-gnu/hdf5/openmpi/ \
+    # must use --with-hdf5-fortran-bindings otherwise can't find 
+    # 	    --with-hdf5-fortran-bindings \
+    # 	    --with-fortran-bindings=1
     # >>> for damask can't use complex
 #	    --with-scalar-type=complex \
     #	    --with-debugging=1 \
